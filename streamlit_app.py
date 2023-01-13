797552f2e9795dd63ae9f719baf4d9477f18d04d
Ruby <31947817+shaheen19@users.noreply.github.com>
2023-01-12 17:37:57 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,5 @@
 import streamlit
-import Panda
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.title('My Parents New Healthy Diner')
 
