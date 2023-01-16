298d53b37e671ec671071662d5bf486c56227758
Ruby <31947817+shaheen19@users.noreply.github.com>
2023-01-16 10:12:45 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-my_fruit_list = my_fruit_list.set_index('Fruit') 
+#my_fruit_list = my_fruit_list.set_index('Fruit') 
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
@@ -19,7 +19,6 @@ streamlit.text('🥑🍞 Avocado-Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # ## Let's put a pick list here so they can pick the fruit they want to include
-#streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberry', 'Apple', 'Grapefruit'])
 fruits_selected= streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberry'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 # ## Display the selected fruits on the page.
