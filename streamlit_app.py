b91ac5b18c29c01acd97b0933cd93f498ab268ad
Ruby <31947817+shaheen19@users.noreply.github.com>
2023-01-31 03:16:29 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,8 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output shown as as table
 streamlit.dataframe(fruityvice_normalized)
-requirements.txt
+
+# from requirements.txt file
 import snowflake.connector
 
 ## Display the table on the page.
