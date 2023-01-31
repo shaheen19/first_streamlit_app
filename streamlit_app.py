42020b26e96c5a208098dbd9a4c5e3223c0546fa
Ruby <31947817+shaheen19@users.noreply.github.com>
2023-01-31 02:53:39 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,8 +42,8 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output shown as as table
 streamlit.dataframe(fruityvice_normalized)
-streatmlit.requirement.txt
-import snowflake.connector
+requirement.txt
+#import snowflake.connector
 
 ## Display the table on the page.
 #streamlit.dataframe(my_fruit_list)
